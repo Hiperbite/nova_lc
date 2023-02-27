cd651a5388836dd74a779e8ebe67fb7ecc07ae97
lutonda <lutonda@gmail.com>
2023-02-27 16:15:35 +0000

    implemented user applciation logics staff

---
+++ src/application/common/user.app.ts
@@ -0,0 +1,43 @@
+import { User } from "../../models/index";
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
+import sendEmail from "../../application/mailler";
+
+export class UserApp {
+  static hashPassword = async (user: User) => {
+/*
+    return;
+    if (
+      (user.changed() || []).filter((x: string) => x === "password").length ===
+      0
+    )
+      return;
+
+    const saltRounds = 10;
+    try {
+      // Generate a salt
+      user.salt = await bcrypt.genSalt(saltRounds);
+
+      // Hash password
+      user.password = await bcrypt.hash(user.password ?? "", user.salt);
+
+      console.log(user.password);
+    } catch (error) {
+      console.log(error);
+    }*/
+  };
+
+  static sendMail = async (user: User) =>
+    await sendEmail({
+      to: user.email,
+      from: "test@example.com",
+      subject: "Verify your email",
+      text: `verification code: ${user.verificationCode}. Id: ${user.id}`,
+    });
+
+  static initVer = async (user: User) => {
+    user.verificationCode = uuid().substring(5, 12).toUpperCase();
+    user.verified = true;
+  };
+  
+}
